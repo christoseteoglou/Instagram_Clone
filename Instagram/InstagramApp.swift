7df891643196a9e267e765693d39656a709ba2d0
christoseteoglou <itsmeglue@gmail.com>
2023-10-17 09:35:50 +0200

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Christos Eteoglou on 2023-10-17.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
